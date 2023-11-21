d112a570ca1ed22b02bcd240c8c8f31a801179d6
swapneel1026 <swapneelshubham44@gmail.com>
2023-11-21 18:00:32 +0530

    fix:styling

--- src/app/components/SideNavbar.tsx
+++ src/app/components/SideNavbar.tsx
@@ -20,14 +20,14 @@ const SideNavbar = () => {
     },
   ];
   return (
-    <div className="overflow-x-hidden sticky top-0 left-0 z-20 flex flex-col items-center  overflow-y-hidded h-screen  w-[3.375rem] text-lg font-[300] text-[#ebecf3] bg-[#232323] ">
-      <p className="p-2 flex font-extrabold text-[1.5rem]">
+    <div className="overflow-x-hidden sticky top-0 left-0 z-20 flex py-2 flex-col items-center  overflow-y-hidded h-screen  w-[70px] text-lg font-[300] text-[#ebecf3] bg-[#000] ">
+      <p className="px-3 py-2 flex font-extrabold text-[1.5rem] bg-[#232323]/60 rounded-lg mt-2 ">
         S <span className="text-[#0aff9d]">.</span>
       </p>
       {links.map((link) => (
         <Link
           key={link.href}
-          className="active:text-7xl transition-all duration-300 hover:opacity-100 opacity-60 rotate-90 border-t-4 border-[#232323] hover:border-t-4 hover:border-[#0aff9d] my-10 py-[0.5625rem] min-w-[7rem]  flex justify-center"
+          className=" transition-all duration-300 hover:opacity-100 opacity-60 rotate-90 border-t-4 border-[#000] hover:border-t-4 hover:border-[#0aff9d] my-10 py-[15px] min-w-[7rem]  flex justify-center"
           href={link.href ?? "/"}
         >
           {link.linkName}

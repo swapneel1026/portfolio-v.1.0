5c9cf43bb3bf601604151436efebd1d09ceb0916
swapneel1026 <swapneelshubham44@gmail.com>
2023-11-21 19:50:47 +0530

    feat:entry point

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
+import HeroSection from "./components/HeroSection";
+import TopNav from "./components/TopNav";
+
 export default function Home() {
   return (
     <>
-      <main>
-        {/* <TopNav /> */}
-        <h1 id="demo" className="mt-[8000px]">
-          Hello
-        </h1>
+      <main className="">
+        <TopNav />
+        <section>
+          <HeroSection />
+        </section>
+        <section></section>
+        <section></section>
+        <section></section>
+        <section></section>
       </main>
     </>
   );

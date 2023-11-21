f0816c0e315cf8a5a362e2cc65a1c4c0a229748b
swapneel1026 <swapneelshubham44@gmail.com>
2023-11-21 20:03:00 +0530

    fix:scrollbar hide

--- src/app/components/SideNavbar.tsx
+++ src/app/components/SideNavbar.tsx
@@ -20,7 +20,7 @@ const SideNavbar = () => {
     },
   ];
   return (
-    <div className="overflow-x-hidden sticky top-0 left-0 z-20 flex py-2 flex-col items-center  overflow-y-hiddn h-screen  min-w-[55px] max-w-[70px] text-lg font-[300] text-[#ebecf3] bg-[#000] ">
+    <div className="overflow-x-hidden scrollbar-hide sticky top-0 left-0 z-20 flex py-2 flex-col items-center  overflow-y-hiddn h-screen  min-w-[55px] max-w-[70px] text-lg font-[300] text-[#ebecf3] bg-[#000] ">
       <p className="px-3 py-2 flex font-extrabold text-[1.5rem] bg-[#232323]/60 rounded-lg mt-2 ">
         S <span className="text-[#0aff9d]">.</span>
       </p>

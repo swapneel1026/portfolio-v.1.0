85e2a1b054b3826bbbda7fb2743d5397d2a24571
swapneel1026 <swapneelshubham44@gmail.com>
2024-04-21 21:15:15 +0530

    feat:new project data

--- src/app/components/ProjectSection.tsx
+++ src/app/components/ProjectSection.tsx
@@ -2,13 +2,22 @@ import { ProjectCard } from "./ProjectCard";
 const ProjectSection = () => {
   const PorjectData = [
     {
-      imageOfProject: "/portfollio-sc.png",
-      name: "PortFolio V.1.0",
-      techStackUsed: ["next.js", "tailwind", "javascript", "Typescript"],
-      desc: "Crafted a sleek portfolio app with Next.js, Tailwind, and a blend of JavaScript and TypeScript for a dynamic touch. The design is not only visually appealing but also responsive, ensuring a seamless user experience across devices. This project showcases my proficiency in modern web development technologies, emphasizing both aesthetic design and functional responsiveness.",
-      githubLink: "https://github.com/swapneel1026/portfolio-v.1.0",
-      liveProjectLink: "https://frontendswapneel.vercel.app/",
+      imageOfProject: "/docterappssbig.png",
+      name: "Book Doctor",
+      techStackUsed: [
+        "react.js",
+        "tailwind",
+        "javascript",
+        "mongodb",
+        "node.js",
+        "express-js",
+      ],
+      desc: "Our project is a user-doctor booking application built with React.js, Tailwind CSS, JavaScript, MongoDB, Node.js, Express.js, and Socket.IO. Users and doctors can sign up, creating personalized profiles for seamless interaction. The platform enables real-time appointment scheduling and efficient communication between patients and healthcare providers, emphasizing intuitive design and robust backend architecture.",
+
+      githubLink: "https://github.com/swapneel1026/docter-app-client",
+      liveProjectLink: "https://bookdoctor.vercel.app/",
     },
+
     {
       imageOfProject: "/ticketsc.png",
       name: "Ticket Tracker",
@@ -25,14 +34,16 @@ const ProjectSection = () => {
       githubLink: "https://github.com/swapneel1026/tickettracker",
       liveProjectLink: "https://tickettracker.vercel.app/",
     },
-    // {
-    //   imageOfProject: "Netflix clone",
-    //   name: "",
-    //   techStackUsed: ["next", "tailwind", "javascript", "Css", "HTML"],
-    //   desc: "Think Zapier but for paint. Built with a team of four college friends and scaled to 1 billion requests per day. This was a fun one.",
-    //   githubLink: "",
-    //   liveProjectLink: "",
-    // },
+    {
+      imageOfProject: "/portfollio-sc.png",
+      name: "PortFolio V.1.0",
+      techStackUsed: ["next.js", "tailwind", "javascript", "Typescript"],
+      desc: "Crafted a sleek portfolio app with Next.js, Tailwind, and a blend of JavaScript and TypeScript for a dynamic touch. The design is not only visually appealing but also responsive, ensuring a seamless user experience across devices. This project showcases my proficiency in modern web development technologies, emphasizing both aesthetic design and functional responsiveness.",
+      githubLink: "https://github.com/swapneel1026/portfolio-v.1.0",
+      liveProjectLink: "https://frontendswapneel.vercel.app/",
+    },
+    
+
   ];
   return (
     <div className="text-white ">
@@ -45,9 +56,9 @@ const ProjectSection = () => {
       </header>
       <section className="gap-[4rem] flex  mt-4 ">
         <div
-          className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ${
+          className={`grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 ${
             PorjectData.length >= 3
-              ? "xl:grid-cols-4"
+              ? "xl:grid-cols-3"
               : PorjectData.length == 2
               ? "xl:grid-cols-2 lg:grid-cols-2"
               : ""
